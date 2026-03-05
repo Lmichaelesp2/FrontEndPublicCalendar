b7beb5749e3050cf1feae01d360a4f4d5bb8365d
Lmichaelesp2 <michael@localbusinesscalendars.com>
2026-03-05 15:06:04 -0600

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -67,7 +67,7 @@ export function Hero() {
                 </div>
               </nav>
               <p className="hero-subtext-below">
-                Calendar access anytime. Always free.
+                Browse the calendar anytime between emails. Always free.
               </p>
             </>
           )}
