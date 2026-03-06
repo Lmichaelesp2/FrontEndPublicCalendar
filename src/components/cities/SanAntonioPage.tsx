9844b153c7fd34f0084cbdfce04fd93ee881bd2c
Lmichaelesp2 <michael@localbusinesscalendars.com>
2026-03-06 15:51:29 -0600

    Updated SanAntonioPage.tsx

--- src/components/cities/SanAntonioPage.tsx
+++ src/components/cities/SanAntonioPage.tsx
@@ -116,7 +116,7 @@ function SanAntonioContent() {
           </p>
           <div className="hero-cta-group">
             <Link to="/san-antonio/subscribe" className="btn btn-white">
-              Sign up for your free weekly email
+              Get My Free San Antonio Events Email
             </Link>
             <p className="hero-subtext-below">
               Browse the calendar anytime between emails. Always free.
